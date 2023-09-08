d809b91a28cda214f0372810c6421fec56c1ebf9
nkwsy <nick@nickwesley.me>
2023-09-08 16:58:06 -0500

    updated

--- download_from_s3.py
+++ download_from_s3.py
@@ -2,30 +2,40 @@ import requests
 import os
 import shutil
 import dotenv
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 dotenv.load_dotenv()
 
-# Define your public S3 bucket URLs here
 HOST_URL = os.getenv("HOST_URL")
 IMG_URL = os.getenv("IMG_URL")
 XML_URL = os.getenv("XML_URL")
 OUT_FOLDER = os.getenv("OUT_FOLDER")
 
-def download_file(url, local_filename):
-    """Download file from a given URL.
+if not all([HOST_URL, IMG_URL, XML_URL, OUT_FOLDER]):
+    logging.error("Environment variables not set correctly.")
+    exit(1)
 
-    Parameters:
-        url (str): URL to download from
-        local_filename (str): Local path where the file will be saved
-    """
-    with requests.get(url, stream=True) as r:
-        with open(local_filename, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192):
-                f.write(chunk)
-    print(f"Downloaded {local_filename}")
+def download_file(url, local_filename):
+    if not url or not local_filename:
+        logging.error(f"Invalid parameters. URL: {url}, Local Filename: {local_filename}")
+        return
+
+    try:
+        with requests.get(url, stream=True) as r:
+            if r.status_code != 200:
+                logging.error(f"Failed to download. HTTP Status Code: {r.status_code}")
+                return
+
+            with open(local_filename, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+        logging.info(f"Downloaded {local_filename}")
+    except Exception as e:
+        logging.error(f"Error in downloading file: {e}")
 
 if __name__ == "__main__":
-    # Define your public S3 bucket URLs here
     files_to_download = {
         IMG_URL: "/home/pi/piink/media/out.jpg",
         XML_URL: "/home/pi/piink/media/dv_signage.xml",
@@ -33,6 +43,9 @@ if __name__ == "__main__":
 
     for url, local_path in files_to_download.items():
         download_file(url, local_path)
-        
-    shutil.move("/home/pi/piink/media/out.jpg", f"{OUT_FOLDER}/out.jpg")
-    shutil.move("/home/pi/piink/media/dv_signage.xml", f"{OUT_FOLDER}/dv_signage.xml")
+
+    try:
+        shutil.move("/home/pi/piink/media/out.jpg", f"{OUT_FOLDER}/out.jpg")
+        shutil.move("/home/pi/piink/media/dv_signage.xml", f"{OUT_FOLDER}/dv_signage.xml")
+    except Exception as e:
+        logging.error(f"Error in moving files: {e}")
