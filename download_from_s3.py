5f0591ce44053c79359d20604e8b65ab424fc827
nkwsy <nick@nickwesley.me>
2023-10-09 21:03:32 -0500

    modify to add datetime filename

--- download_from_s3.py
+++ download_from_s3.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import dotenv
 import logging
+import datetime
 
 logging.basicConfig(filename='/home/pi/piink/piink.log', level=logging.INFO)
 
@@ -17,17 +18,30 @@ if not all([HOST_URL, IMG_URL, XML_URL, OUT_FOLDER]):
     logging.error("Environment variables not set correctly.")
     exit(1)
 
+def replace_img_in_xml(xml_path, img_name):
+    if not xml_path or not img_name:
+        logging.error(f"Invalid parameters. XML Path: {xml_path}, Image Path: {img_name}")
+        return
+
+    try:
+        with open(xml_path, 'r') as f:
+            xml = f.read()
+            xml = xml.replace("out", img_name)
+        with open(xml_path, 'w') as f:
+            f.write(xml)
+        logging.info(f"Replaced image path in {xml_path}")
+    except Exception as e:
+        logging.error(f"Error in replacing image path: {e}")
+
 def download_file(url, local_filename):
     if not url or not local_filename:
         logging.error(f"Invalid parameters. URL: {url}, Local Filename: {local_filename}")
         return
-
     try:
         with requests.get(url, stream=True) as r:
             if r.status_code != 200:
                 logging.error(f"Failed to download. HTTP Status Code: {r.status_code}")
                 return
-
             with open(local_filename, 'wb') as f:
                 for chunk in r.iter_content(chunk_size=8192):
                     f.write(chunk)
@@ -36,15 +50,16 @@ def download_file(url, local_filename):
         logging.error(f"Error in downloading file: {e}")
 
 if __name__ == "__main__":
+    filename = {datetime.datetime.now().strftime("%Y%m%d%H%M%S")}
     files_to_download = {
         IMG_URL: "/home/pi/piink/media/out.jpg",
         XML_URL: "/home/pi/piink/media/dv_signage.xml",
     }
-
     try:
         for url, local_path in files_to_download.items():
             download_file(url, local_path)
-        shutil.move("/home/pi/piink/media/out.jpg", f"{OUT_FOLDER}/out.jpg")
+        replace_img_in_xml("/home/pi/piink/media/dv_signage.xml", filename)
+        shutil.move("/home/pi/piink/media/out.jpg", f"{OUT_FOLDER}/{filename}.jpg")
         shutil.move("/home/pi/piink/media/dv_signage.xml", f"{OUT_FOLDER}/dv_signage.xml")
     except Exception as e:
         logging.error(f"Error in moving files: {e}")
