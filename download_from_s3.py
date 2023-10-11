e8598eb981cf5d7f0331d6873713edc681e431a9
nkwsy <nick@nickwesley.me>
2023-10-11 13:51:43 -0500

    add webhook

--- download_from_s3.py
+++ download_from_s3.py
@@ -4,6 +4,7 @@ import shutil
 import dotenv
 import logging
 import datetime
+import json
 
 logging.basicConfig(filename='/home/pi/piink/piink.log', level=logging.INFO)
 
@@ -13,6 +14,17 @@ HOST_URL = os.getenv("HOST_URL")
 IMG_URL = os.getenv("IMG_URL")
 XML_URL = os.getenv("XML_URL")
 OUT_FOLDER = os.getenv("OUT_FOLDER")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+
+def send_slack_notification(message):
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    data = {
+        'text': message
+    }
+    response = requests.post(WEBHOOK_URL, data=json.dumps(data), headers=headers)
+    return response
 
 if not all([HOST_URL, IMG_URL, XML_URL, OUT_FOLDER]):
     logging.error("Environment variables not set correctly.")
@@ -32,6 +44,7 @@ def replace_img_in_xml(xml_path, img_name):
         logging.info(f"Replaced image path in {xml_path}")
     except Exception as e:
         logging.error(f"Error in replacing image path: {e}")
+        send_slack_notification(f"PiInk - Error in replacing image path: {e}")
 
 def download_file(url, local_filename):
     if not url or not local_filename:
@@ -48,6 +61,7 @@ def download_file(url, local_filename):
         logging.info(f"Downloaded {local_filename}")
     except Exception as e:
         logging.error(f"Error in downloading file: {e}")
+        send_slack_notification(f"PiInk - Error in downloading file: {e}")
 
 if __name__ == "__main__":
     filename = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
@@ -61,5 +75,7 @@ if __name__ == "__main__":
         replace_img_in_xml("/home/pi/piink/media/dv_signage.xml", filename)
         shutil.move("/home/pi/piink/media/out.jpg", f"{OUT_FOLDER}/{filename}.jpg")
         shutil.move("/home/pi/piink/media/dv_signage.xml", f"{OUT_FOLDER}/dv_signage.xml")
+        send_slack_notification(f"PiInk - Successfully moved files to {OUT_FOLDER}")
     except Exception as e:
         logging.error(f"Error in moving files: {e}")
+        send_slack_notification(f"PiInk - Error in moving files: {e}")
